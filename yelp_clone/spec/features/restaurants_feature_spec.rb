f50cc831bd766e69206d61c7cf5109473232f401
Winifred Au <winifredau1990@gmail.com>
2015-09-01 08:54:37 -0400

    created Restaurant table

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,12 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+  context 'Restaurants have been added' do
+    before(:each) {Restaurant.create(name: 'KFC')}
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+  end
 end
