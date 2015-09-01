fb3400ac5190e76abb9173b2900485832e5fb916
Josh <Tattsy@hotmail.co.uk>
2015-09-01 14:28:37 +0100

    can add restaurants on front end

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -16,4 +16,14 @@ feature 'restaurants' do
       expect(page).not_to have_content 'No restaurants yet'
     end
   end
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out form, then displays the new restaurant' do
+      visit 'restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
 end
