3cea90cbbdc38ca3c2e19274940305423e7d1356
Ajitsy <sanghera.p@gmail.com>
2015-09-02 11:38:23 +0100

    created working tests for deleting associated reviews when removing restaurant

--- yelp_clone/spec/models/restaurant_spec.rb
+++ yelp_clone/spec/models/restaurant_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews}
-end 
+end
+
+describe Restaurant, type: :model do
+  let(:kfc) {Restaurant.create(name: 'KFC' )}
+  it "deletes associated reviews when deleting a restaurant" do
+    review = Review.create(thoughts: 'test review', rating: 3, restaurant_id: kfc.id)
+    kfc.save
+    expect{kfc.destroy}.to change(Review, :count).by(-1)
+    expect{Review.find(review.id)}.to raise_error "Couldn't find Review with 'id'=#{review.id}" 
+  end
+end
