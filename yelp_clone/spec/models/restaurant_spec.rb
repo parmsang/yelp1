a81b89f37b62cc64b50f62d222cf1ad48f3a090e
Ajitsy <sanghera.p@gmail.com>
2015-09-02 13:38:22 +0100

    a refactor

--- yelp_clone/spec/models/restaurant_spec.rb
+++ yelp_clone/spec/models/restaurant_spec.rb
@@ -9,7 +9,7 @@ describe Restaurant, type: :model do
   it "deletes associated reviews when deleting a restaurant" do
     review = Review.create(thoughts: 'test review', rating: 3, restaurant_id: kfc.id)
     expect{kfc.destroy}.to change(Review, :count).by(-1)
-    expect{Review.find(review.id)}.to raise_error "Couldn't find Review with 'id'=#{review.id}" 
+    expect{Review.find(review.id)}.to raise_error "Couldn't find Review with 'id'=#{review.id}"
   end
   it 'is not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name: "kf")
